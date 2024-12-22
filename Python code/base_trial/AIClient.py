49e3e75acc50595dcd4d016be9d7502d75e56f58
helloKittyCatSweet <1448185924@qq.com>
2024-12-22 21:13:45 +0800

    python files

---
+++ Python code/base_trial/AIClient.py
@@ -0,0 +1,54 @@
+import storyFromAI
+
+# 以下密钥信息从控制台获取
+appid = "c953c109"  # 填写控制台中获取的 APPID 信息
+api_secret = "NWY5ODBkODQ1ODIzMGE0ZTM5ZjhkM2Q2"  # 填写控制台中获取的 APISecret 信息
+api_key = "b904b234790459bb1cc32fe35fcacde8"  # 填写控制台中获取的 APIKey 信息
+
+# 调用微调大模型时，设置为“patch”
+domain = "xqwen257bchat"
+
+# 云端环境的服务地址
+Spark_url = "wss://maas-api.cn-huabei-1.xf-yun.com/v1.1/chat"  # 微调v1.5环境的地址
+# Spark_url = "wss://spark-api-n.xf-yun.com/v3.1/chat"  # 微调v3.0环境的地址
+
+
+text = []
+
+
+# length = 0
+
+def getText(role, content):
+    jsoncon = {}
+    jsoncon["role"] = role
+    jsoncon["content"] = content
+    text.append(jsoncon)
+    return text
+
+
+def getlength(text):
+    length = 0
+    for content in text:
+        temp = content["content"]
+        leng = len(temp)
+        length += leng
+    return length
+
+
+def checklen(text):
+    while (getlength(text) > 8000):
+        del text[0]
+    return text
+
+
+if __name__ == '__main__':
+    text.clear
+    while (1):
+        Input = input("\n" + "我:")
+        question = checklen(getText("user", Input))
+        storyFromAI.answer = ""
+        print("星火:", end="")
+        storyFromAI.main(appid, api_key, api_secret, Spark_url, domain, question)
+        getText("assistant", storyFromAI.answer)
+        # print(str(text))
+
